5e9b7d5a094f9b66c90a3777f03ea43cb0431431
Arunendra Singh <arunendrasingh11@live.com>
2020-03-04 20:03:06 +0530

    Update sample-registration-form-validation.js

--- sample-registration-form-validation.js
+++ sample-registration-form-validation.js
@@ -5,8 +5,19 @@ function userid_validation(uid,mx,my){
         alert("User Id should not be empty / length be between "+mx+" to "+my);
         uid.focus();
         return false;
-    }
+    };
+
 return true;
+}
+
+function passid_validation(passid, mx, my) {
+    var passid_len = passid.value.length;
+    if (passid_len == 0 || passid_len >= my || passid_len < mx) {
+        alert("Password should not be empty / length be between " + mx + " to " + my);
+        passid.focus();
+        return false;
+    }
+    return true;
 }
 
 // Apply function on program file
